2da8547aee89d6cd32a86067b273937f68891516
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-08-14 13:29:10 +0200

    add gridsome pages

---
+++ src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/docs">Docs</Link>
+        </li>
+        <li>
+          <Link to="/get-started">Get started</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <slot/>
+  </div>
+</template>
+
+<script>
+import { Link } from '@gridsome/components'
+
+export default {
+  components: {
+    Link
+  }
+}
+</script>
