78b6ad036cb8ddeec193c1a7c12ed9f632023fa7
Tommy Vedvik <tommyvedvik@gmail.com>
2018-08-16 13:09:27 +0200

    Added typography.js + typeface spartan

--- src/main.js
+++ src/main.js
@@ -8,3 +8,17 @@ import Docs from '@/layouts/Docs.vue'
 Vue.component('Link', Link)
 Vue.component('Layout', Layout)
 Vue.component('DocsLayout', Docs)
+
+
+// Setup typography
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  headerFontFamily: ['League Spartan', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Georgia', 'serif'],
+})
+
+typography.injectStyles()
+require('typeface-league-spartan')
