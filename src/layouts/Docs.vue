a161bb70c3c51a9abb17de84d29d7ed126ec865b
Tommy Vedvik <tommyvedvik@gmail.com>
2018-08-27 21:00:29 +0200

    Docs structure draft

--- src/layouts/Docs.vue
+++ src/layouts/Docs.vue
@@ -4,7 +4,50 @@
       <div class="sidebar dots-bg">
         
         <input type="search" placeholder="Search docs..." />
+
+        <h3>
+          Gettings started
+        </h3>
+        <p>Install CLI</p>
+        <p>Starter kits</p>
+
+        <h3>
+          Core concepts
+        </h3>
+        <p>How Gridsome works</p>
+        <p>Pages, layouts & templates</p>
+        <p>Vue Components</p>
+        <p>Data Sources</p>
+        <p>Images & Links</p>
+        <p>Static Site Generation</p>
+        <p>Hosting & Deploying</p>
+
+        <h3> Guides </h3>
+        <p>Install plugins</p>
+        <p>Add Critical CSS</p>
+        <p>Add custom fonts</p>
+        <p>How to add pagination</p>
+        <p>Add a Source template</p>
+
+        <h3>API Reference</h3>
+        <p>CLI</p>
+        <p>Config</p>
+        <p>Head</p>
+        <p>Pages</p>
+        <p>Plugins</p>
+        <p>Components</p>
+
+        <h3>Global Components</h3>
+        <p>Link</p>
+        <p>Image</p>
+        <p>Pagination</p>
+        <p>Slots</p>
+
+        <h3>Deploy</h3>
         
+        <p>Netlify</p>
+        <p>Zeit</p>
+
         <ul>
           <li v-for="{ node } in $static.docs.edges">
             <g-link :to="node.path">
@@ -26,6 +69,17 @@
     </div>
   </Layout>
 </template>
+
+<style>
+.sidebar h3 {
+  font-size: 16px;
+  margin: 20px 0 15px;
+}
+.sidebar p {
+  margin-bottom: 5px;
+}
+
+</style>
 
 <static-query>
 query Docs {
--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -1,7 +1,53 @@
 <template>
   <DocsLayout>
     <h1>Get started with Gridsome</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores ab harum aliquid dignissimos neque.</p>
+    <p>Gridsome is a framework for building blazing-fast websites with Vue.js and GraphQL. This guide will help you get quickly up and running with your first Gridsome site.</p>
+    <h3>Install the Gridsome CLI tool</h3>
+    <code>npm install --global gridsome-cli</code><br>
+    <p><small><i>If you're not sure where to put this we recommend you follow the Tutorial.</i></small></p>
+    <h3>Start use the CLI</h3>
+    <ol>
+      <li>Create a new site: <code>gridsome create my-gridsome-site</code> </li>
+      <li><code>cd my-gridsome-site</code></li>
+      <li><code>gridsome develop</code> to start local dev server at <code>localhost:8080</code></li>
+      <li>Happy coding 🙌</li>
+    </ol>
+    <h3>Next steps...</h3>
+    <p>Learn about the core concepts of building Gridsome sites</p>
+    <div class="grid-cols grid-cols--2 grid-cols--gap-small">
+      <div> 
+        <h4 style="margin-bottom: 10px;">How Gridsome works</h4>
+        <p>Detailed explenation of how Gridsome works</p>
+      </div>
+      <div> 
+        <h4 style="margin-bottom: 10px;">Pages, layouts & templates</h4>
+        <p>Learn how to add pages and layouts to Gridsome</p>
+      </div>
+      <div> 
+        <h4 style="margin-bottom: 10px;">Vue Components</h4>
+        <p>Learn the concept of Vue and its Single File Components</p>
+      </div>
+      <div>
+        <h4 style="margin-bottom: 10px;">Data Sources</h4>
+        <p>Learn how to add source plugins and get data with GraphQL</p>
+      </div>
+      
+      <div> 
+        <h4 style="margin-bottom: 10px;">Images & Links</h4>
+        <p>Learn how to use the built-in Image & link components</p>
+      </div>
+
+      <div> 
+        <h4 style="margin-bottom: 10px;">Static Site Generation</h4>
+        <p>Learn how Gridsome builds and optimize the website</p>
+      </div>
+  
+      <div> 
+        <h4 style="margin-bottom: 10px;">Hosting & Deploying</h4>
+        <p>Learn how to generate, deploy and host your website</p>
+      </div>
+    </div>
+
   </DocsLayout>
 </template>
 
@@ -14,3 +60,11 @@ export default {
   }
 }
 </script>
+
+<style>
+.grid-cols div {
+  padding: 30px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
