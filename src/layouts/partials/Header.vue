5226c9d74e7ea02180f08d0d244aed8f1fa15aad
Tommy Vedvik <tommyvedvik@gmail.com>
2018-09-15 16:04:32 +0200

    added discord and github to header

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -4,63 +4,62 @@
 
     <Nav class="flex-fit" />
     
-    <div class="header-actions flex gap-15">
-    
-    <a aria-label="Visit Gridsome on GitHub" href="https://github.com/gridsome/gridsome">
-      <GithubLogo />
-    </a>
+    <nav class="header-actions flex gap-15">
 
-    <g-link to="/docs" class="button small hide-for-small">
-      Get started
-    </g-link>
+      <a href="//discord.gg/daeay6n">
+        Discord
+      </a>
+
+      <a href="//github.com/gridsome/gridsome">
+        GitHub
+      </a>
+
+      <g-link to="/docs" class="button small hide-for-small">
+        Get started
+      </g-link>
+
+    </nav>
+
   
-    </div>
   </header>
 </template>
 
 <script>
 import Logo from './Logo'
 import Nav from './Nav'
-import GithubLogo from '@/components/logos/github'
 
 export default {
   components: {
     Logo,
     Nav,
-    GithubLogo
   }
 }
 </script>
 
 <style lang="scss">
-  header {
-    z-index: 20;
-    position: relative;
+header {
+  z-index: 20;
+  position: relative;
+  top:0;
+  min-height: var(--header-height);
+  background-color: #FFF;
+  border-bottom: 1px solid transparent;
+  
+  .has-sidebar & {
+    border-bottom: 1px solid var(--border-color);
+    position: fixed;
     top:0;
-    min-height: var(--header-height);
-    background-color: #FFF;
-    border-bottom: 1px solid transparent;
-    
-    .has-sidebar & {
-      border-bottom: 1px solid var(--border-color);
-      position: fixed;
-      top:0;
-    }
   }
+}
 
-  @media screen and (max-width: 750px) {
-    header {
-      box-shadow: 0 0 10px 0 rgba(0,0,0,.15);
-    }
+
+@media screen and (max-width: 750px) {
+  header {
+    box-shadow: 0 0 10px 0 rgba(0,0,0,.15);
   }
-
   .header-actions {
     margin-left: auto!important;
+    font-size: .9rem;
   }
-
-  .github-logo {
-    max-width: 20px;
-    height: 20px;
-    margin-top: -6px;
-  }
+}
 </style>
--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="main-nav flex gap-20">
+  <nav class="main-nav flex gap-15">
 
     <g-link to="/docs">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -27,30 +27,6 @@
 </template>
 
 <style lang="scss">
-  .main-nav a {
-    letter-spacing: .5px;
-    font-size: .9em;
-    font-weight: 500;
-    display: flex;
-    align-items:center;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding: 5px;
-    svg {
-      margin: 3px 6px;
-      opacity: .5;
-    }
-    &.active {
-      svg {
-        opacity: 1;
-      }
-    }
-  }
-  .main-nav a:not(.active):not(:hover) {
-    color: currentColor;
-    opacity: .7;
-  }
-
 
   @media screen and (max-width: 750px) {
     .main-nav {
@@ -65,8 +41,6 @@
       min-height: 60px;
       flex-wrap: nowrap!important;
       padding: 10px var(--space);
-
- 
     }
   }
 </style>
