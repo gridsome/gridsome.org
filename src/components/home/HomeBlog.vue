c2935596212c18502d40bec8fbd4be1c7eded734
Tommy Vedvik <tommyvedvik@gmail.com>
2018-09-24 10:50:36 +0200

    duplicate style tag

--- src/components/home/HomeBlog.vue
+++ src/components/home/HomeBlog.vue
@@ -39,34 +39,3 @@ query BlogPosts {
   }
 }
 </static-query>
-
-
-<style lang="scss">
-  .feature {
-    text-align:center; 
-
-    &__icon {
-      width: 50px;
-      height: 50px;
-      border-radius: 99px;
-      margin: 0 auto 30px;
-      background-color: #FFF;
-      display:flex;
-      align-items:center;
-      justify-content:center;
-      svg {
-        margin-top:-3px;
-      }
-    }
-
-    &__inner {
-      flex: 1;
-    }
-
-    h3 {
-      margin-bottom: 1rem;
-      padding: 0 1rem;
-      line-height: 1.4;
-    }
-  }
-</style>
