b1f3bd43c06fcc08f3ffef2c07d8cb3b808b4108
Tommy Vedvik <tommyvedvik@gmail.com>
2018-09-28 13:21:39 +0200

    structure html properly

--- src/components/home/HomeHowItWorks.vue
+++ src/components/home/HomeHowItWorks.vue
@@ -10,13 +10,15 @@
       <div class="flex flex--align-equal flex--space-between">
           <div class="well">
               <div class="well__title">Data sources</div>
-              <p><b>CMSs</b> <br>WordPress, Drupal,
+              <h4>CMSs</h4>
+              <p>WordPress, Drupal,
               Contentful, Shopify, Magento etc.</p>
 
-              <p><b>Files</b> <br>
-              Markdown, YAML, CSV, JSON, Image folders, etc</p>
+              <h4>Files</h4>
+              <p>Markdown, YAML, CSV, JSON, Image folders, etc</p>
 
-              <p><b>APIs</b> <br>AirTable, Google Spreadsheet, MongoDB, Firebase etc.</p>
+              <h4>APIs</h4>
+              <p>AirTable, Google Spreadsheet, MongoDB, Firebase etc.</p>
           </div>
           <div class="dots hide-for-small">
               <svg  width="100%" height="20" viewBox="0 0 60 20" class="dot">
@@ -28,19 +30,19 @@
               <div class="layers">
                 <div class="layer layer__graphql text-center">
                   <div class="text-wrapper">
-                    <span class="text">
+                    <div class="text">
                       <graph-ql-logo width="50px" height="50px"  style="color:#fff;" /><br/>
                       GraphQL <br>data layer
-                    </span>
+                    </div>
                   </div>
                 </div>
                 <div class="layer layer__vue">
                   <div class="text-wrapper">
-                    <span class="text text-center">
+                    <div class="text text-center">
                         <strong>Vue.js</strong><br/>
                         Webpack<br/>
                         Hot-reloading<br/>
-                    </span>
+                    </div>
                  </div>
                  <div class="dots-bg"></div>
                 </div>
@@ -53,13 +55,12 @@
           </div>
           <div class="well">
               <div class="well__title">Deploy</div>
+                <h4>Static site generation</h4>
               <p>
-                <strong>Static site generation</strong><br>
-                Generate static files and deploy to any CDN service. Netlify, Zeit Now, GitHub Pages, GitLab Pages, Surge.sh, Now.sh and many more.
-              </p>
+                Generate static files and deploy to any CDN service. For example, Netlify, Zeit Now, GitHub Pages, GitLab Pages, Surge.sh, Now.sh and many more.</p>           
 
+              <h4>Server-side rendering</h4>
               <p>
-                <strong>Server-side rendering</strong><br>
                 Deploy Gridsome and the GraphQL layer to any Node.js server
               </p>
 
@@ -84,8 +85,14 @@ export default {
   padding: var(--space-x2);
   position: relative;
   flex:1;
-  min-width: 300px;
+  min-width: 280px;
   margin-bottom: var(--space-x2);
+
+  h4{
+    margin-bottom: .5rem;
+    font-size: .9rem;
+  }
+
   &__title {
     position: absolute;
     top:-19px;
@@ -98,7 +105,7 @@ export default {
     font-size: .85rem;
     letter-spacing: 1px;
     font-weight: 600;
-    }
+  }
 }
 
 .layers {
@@ -123,7 +130,6 @@ export default {
   position: absolute;
   top: 42%;
   animation: stroke 400ms linear infinite;
-
   &--after {
     stroke: var(--primary-color);
   }
@@ -139,7 +145,6 @@ export default {
   &__graphql {
     background: linear-gradient(-80deg, #E535AB 0%, rgba(229, 53, 171, 0.49) 100%);
     animation: pulseAlt 2s infinite;
-
     position: relative;
     padding-top: 80%;
     width: 80%;
