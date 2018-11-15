567e3b24dc905cd136ab7ba5e377c095b964050e
Tommy Vedvik <tommyvedvik@gmail.com>
2018-11-15 21:39:39 +0100

    Update to "Bring content from anywhere" title

--- src/components/home/HomeFeatures.vue
+++ src/components/home/HomeFeatures.vue
@@ -4,8 +4,8 @@
 
       <Feature>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" class="icon feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
-
-        <h3>Connect to any CMS or data source</h3>
+        
+        <h3>Bring content from anywhere</h3>
         <p>Use any CMS or data source for content. Pull data from WordPress, Contentful, local Markdown, or any other headless CMS or APIs and access it with <strong>GraphQL<graph-ql-logo /></strong> in your pages and components.
         </p>
       </Feature>
