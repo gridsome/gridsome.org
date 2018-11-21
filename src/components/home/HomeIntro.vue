0c7ae51074e00e729bcdc712094110c22bb70801
Tommy Vedvik <tommyvedvik@gmail.com>
2018-11-21 18:08:51 +0100

    Add license and github link to top

--- src/components/home/HomeIntro.vue
+++ src/components/home/HomeIntro.vue
@@ -11,17 +11,19 @@
     <p class="home-lead lead  container-sm">
       Gridsome is a blazing-fast <strong>Vue-powered static site generator</strong> for building front-ends for any headless CMS or data
     </p>
+    <p class="home-links">
+      <g-link  to="/docs" class="button primary">
+        Get started
+      </g-link>
+    </p>  
 
-    <g-link  to="/docs" class="button primary">
-      Get started
-    </g-link>
+    <p class="home-info">
+      License: MIT. <a href="https://github.com/gridsome/gridsome">GitHub (0.3.3)</a> 
+    </p>
 
-    <a href="//github.com/gridsome/gridsome" class="button plain">
-      GitHub
-    </a>
   </div>
 
-  <logo-row style="margin-bottom: -30px; padding-top: 50px" />
+  <logo-row style="margin-bottom: -30px; " />
 
 
   </Section>
