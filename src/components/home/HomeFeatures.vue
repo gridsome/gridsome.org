61f7f55e444383aaf5fb0503a8b25e9d53d47d5e
Tommy Vedvik <tommyvedvik@gmail.com>
2018-12-17 13:44:21 +0100

    Update to fast by default

--- src/components/home/HomeFeatures.vue
+++ src/components/home/HomeFeatures.vue
@@ -27,7 +27,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" class="icon feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
 
         <h3>Fast by default</h3>
-        <p>Gridsome automatically optimizes your frontend to load and perform fast as possible. You get code-splitting, image optimization, and progressive image support out-of-the-box.
+        <p>Gridsome automatically optimizes your frontend to load and perform fast as possible. You get <strong>code-splitting, page prefetching, and progressive image loading</strong> out-of-the-box.
         </p>     
       </Feature>
 
