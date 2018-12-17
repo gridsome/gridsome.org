cb1ab02623b0d771c7d0420dc2a8e82bba515d97
Tommy Vedvik <tommyvedvik@gmail.com>
2018-12-17 20:43:56 +0100

    Fix image

--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="main-nav flex gap-15">
+
+    <!-- 
     <a class="hide-for-small">
       <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-    </a>
+    </a> -->
 
     <g-link to="/docs">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="show-for-small feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
