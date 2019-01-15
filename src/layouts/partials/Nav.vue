d3c5c4fcbdc164585a16bbd586e80dfae9256de5
Tommy Vedvik <tommyvedvik@gmail.com>
2019-01-15 11:13:32 +0100

    Move icons to its own files

--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -1,106 +1,49 @@
 <template>
   <nav class="main-nav flex gap-15">
     <g-link to="/docs">
-      <svg
-        class="show-for-small feather feather-file-text"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-        <polyline points="14 2 14 8 20 8"></polyline>
-        <line
-          x1="16"
-          x2="8"
-          y1="13"
-          y2="13"
-        ></line>
-        <line
-          x1="16"
-          x2="8"
-          y1="17"
-          y2="17"
-        ></line>
-        <polyline points="10 9 9 9 8 9"></polyline>
-      </svg>
+      <icon-docs />
       <span class="main-nav__label">Docs</span>
     </g-link>
 
     <g-link to="/learn">
-      <svg
-        class="show-for-small feather feather-compass"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="12"
-          cy="12"
-          r="10"
-        ></circle>
-        <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
-      </svg>
+      <icon-learn />
       <span class="main-nav__label">Learn</span>
     </g-link>
 
     <g-link to="/plugins">
-      <svg
-        class="show-for-small feather feather-zap"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-      </svg>
+      <icon-plugins />
       <span class="main-nav__label">Plugins</span>
     </g-link>
 
     <g-link to="/blog">
-      <svg
-        class="show-for-small feather feather-edit"
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
-        <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
-      </svg>
+      <icon-blog />
       <span class="main-nav__label">Blog</span>
     </g-link>
   </nav>
 </template>
 
 <script>
-export default {}
+import IconPlugins from '@/assets/images/icon-plugins.svg'
+import IconBlog from '@/assets/images/icon-blog.svg'
+import IconDocs from '@/assets/images/icon-docs.svg'
+import IconLearn from '@/assets/images/icon-learn.svg'
+
+export default {
+  components: {
+    IconPlugins,
+    IconLearn,
+    IconDocs,
+    IconBlog
+  }
+}
 </script>
 
 <style lang="scss">
 .main-nav {
   margin-top: 3px;
+  svg {
+    display: none;
+  }
 
   &__label {
     text-transform: uppercase;
@@ -124,6 +67,10 @@ export default {}
     min-height: 60px;
     flex-wrap: nowrap !important;
     padding: 5px var(--space);
+
+    svg {
+      display: block;
+    }
   }
 }
 @media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
