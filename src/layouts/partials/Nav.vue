eafa519040d0e27c8cc8935091ec84c545e27459
Lukas Lusiak <ll_code@outlook.com>
2019-01-15 11:18:06 +0100

    fix: Header&Navbar Changes (#49)
    * Header&Navbar Changes
    
    1. Remove dropdown in header, it has the same content as menu: twitter, discord, blog.
    2. Move Search to right, no menu collapse now.
    3. Items in Navbar Menu changed to uppercased.
    4. Header height set to 62px.
    
    * Delete package-lock.json
    
    * Make search bar always visible
    
    * Remove double quotes and remove the semicolons
    
    * Move to Single Quotes and Remove Semicolons
    
    * Set Rules for Semicolons and Quotes
    
    * Merge branch 'master' into pr/49
    
    * Move icons to its own files

--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -1,65 +1,81 @@
 <template>
   <nav class="main-nav flex gap-15">
-
-    <SearchForm class="hide-for-small"  />
-
     <g-link to="/docs">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="show-for-small feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-      <span>Docs</span>
+      <icon-docs />
+      <span class="main-nav__label">Docs</span>
     </g-link>
 
     <g-link to="/learn">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="show-for-small feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
-      <span>Learn</span>
+      <icon-learn />
+      <span class="main-nav__label">Learn</span>
     </g-link>
 
     <g-link to="/plugins">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="show-for-small feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
-      <span>Plugins</span>
+      <icon-plugins />
+      <span class="main-nav__label">Plugins</span>
     </g-link>
 
     <g-link to="/blog">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="show-for-small feather feather-edit"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>
-      <span>Blog</span>
+      <icon-blog />
+      <span class="main-nav__label">Blog</span>
     </g-link>
-
-
   </nav>
 </template>
 
 <script>
-import SearchForm from '~/components/SearchForm.vue'
+import IconPlugins from '@/assets/images/icon-plugins.svg'
+import IconBlog from '@/assets/images/icon-blog.svg'
+import IconDocs from '@/assets/images/icon-docs.svg'
+import IconLearn from '@/assets/images/icon-learn.svg'
 
 export default {
   components: {
-    SearchForm
+    IconPlugins,
+    IconLearn,
+    IconDocs,
+    IconBlog
   }
 }
 </script>
 
 <style lang="scss">
-  @media screen and (max-width: 750px) {
-    .main-nav {
-      position: fixed;
-      bottom:0;
-      left:0;
-      right:0;
-      background: var(--primary-bg);
-      color: var(--secondary-bg);
-      margin-right:0!important;
-      justify-content: center;
-      box-shadow: 0 0 10px 0 rgba(0,0,0,.2);
-      min-height: 60px;
-      flex-wrap: nowrap!important;
-      padding: 5px var(--space);
+.main-nav {
+  margin-top: 3px;
+  svg {
+    display: none;
+  }
+
+  &__label {
+    text-transform: uppercase;
+    font-size: 0.78rem;
+    letter-spacing: 1px;
+    font-weight: 300;
+  }
+}
+
+@media screen and (max-width: 750px) {
+  .main-nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: var(--primary-bg);
+    color: var(--secondary-bg);
+    margin-right: 0 !important;
+    justify-content: center;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    min-height: 60px;
+    flex-wrap: nowrap !important;
+    padding: 5px var(--space);
+
+    svg {
+      display: block;
     }
   }
-  @media only screen 
-  and (device-width : 375px) 
-  and (device-height : 812px) 
-  and (-webkit-device-pixel-ratio : 3) {
-    .main-nav {
-      padding-bottom: var(--space);
-    }
+}
+@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+  .main-nav {
+    padding-bottom: var(--space);
   }
+}
 </style>
