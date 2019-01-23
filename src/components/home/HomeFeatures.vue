40df0e1019e5f148f802b73ecef99e07e64a5bb2
Tommy Vedvik <tommyvedvik@gmail.com>
2019-01-23 09:41:08 +0100

    fix: remove icons on homepage

--- src/components/home/HomeFeatures.vue
+++ src/components/home/HomeFeatures.vue
@@ -3,34 +3,25 @@
     <div class="homepage-features grid-cols">
 
       <Feature>
-        <svg alt="GraphQL" class="icon" fill="currentColor" width="20px" height="20px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 400 400" enable-background="new 0 0 400 400" xml:space="preserve"> <g> <g> <g> <rect x="122" y="-0.4" transform="matrix(-0.866 -0.5 0.5 -0.866 163.3196 363.3136)" fill="currentColor" width="16.6" height="320.3"/> </g> </g> <g> <g> <rect x="39.8" y="272.2" fill="currentColor" width="320.3" height="16.6"/> </g> </g> <g> <g> <rect x="37.9" y="312.2" transform="matrix(-0.866 -0.5 0.5 -0.866 83.0693 663.3409)" fill="currentColor" width="185" height="16.6"/> </g> </g> <g> <g> <rect x="177.1" y="71.1" transform="matrix(-0.866 -0.5 0.5 -0.866 463.3409 283.0693)" fill="currentColor" width="185" height="16.6"/> </g> </g> <g> <g> <rect x="122.1" y="-13" transform="matrix(-0.5 -0.866 0.866 -0.5 126.7903 232.1221)" fill="currentcolor" width="16.6" height="185"/> </g> </g> <g> <g> <rect x="109.6" y="151.6" transform="matrix(-0.5 -0.866 0.866 -0.5 266.0828 473.3766)" fill="currentColor" width="320.3" height="16.6"/> </g> </g> <g> <g> <rect x="52.5" y="107.5" fill="currentColor" width="16.6" height="185"/> </g> </g> <g> <g> <rect x="330.9" y="107.5" fill="currentColor" width="16.6" height="185"/> </g> </g> <g> <g> <rect x="262.4" y="240.1" transform="matrix(-0.5 -0.866 0.866 -0.5 126.7953 714.2875)" fill="currentColor" width="14.5" height="160.9"/> </g> </g> <path  d="M369.5,297.9c-9.6,16.7-31,22.4-47.7,12.8c-16.7-9.6-22.4-31-12.8-47.7c9.6-16.7,31-22.4,47.7-12.8 C373.5,259.9,379.2,281.2,369.5,297.9"/> <path fill="currentColor" d="M90.9,137c-9.6,16.7-31,22.4-47.7,12.8c-16.7-9.6-22.4-31-12.8-47.7c9.6-16.7,31-22.4,47.7-12.8 C94.8,99,100.5,120.3,90.9,137"/> <path fill="currentColor" d="M30.5,297.9c-9.6-16.7-3.9-38,12.8-47.7c16.7-9.6,38-3.9,47.7,12.8c9.6,16.7,3.9,38-12.8,47.7 C61.4,320.3,40.1,314.6,30.5,297.9"/> <path fill="currentColor" d="M309.1,137c-9.6-16.7-3.9-38,12.8-47.7c16.7-9.6,38-3.9,47.7,12.8c9.6,16.7,3.9,38-12.8,47.7 C340.1,159.4,318.7,153.7,309.1,137"/> <path fill="currentColor" d="M200,395.8c-19.3,0-34.9-15.6-34.9-34.9c0-19.3,15.6-34.9,34.9-34.9c19.3,0,34.9,15.6,34.9,34.9 C234.9,380.1,219.3,395.8,200,395.8"/> <path fill="currentColor" d="M200,74c-19.3,0-34.9-15.6-34.9-34.9c0-19.3,15.6-34.9,34.9-34.9c19.3,0,34.9,15.6,34.9,34.9 C234.9,58.4,219.3,74,200,74"/> </g> </svg>
-
         <h3>Bring your own data</h3>
         <p>Load data from any CMS, APIs, static files, a database, or from multiple places at once. Gridsome combines them all into a simple, centralized <g-link to="/docs/how-it-works">GraphQL data layer</g-link> that pages and components can access.</p>
-        </p>
       </Feature>
 
 
 
       <Feature>
-        <svg class="icon" width="32" height="32" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 485.61 485.61" xml:space="preserve" fill="currentcolor"><g id="XMLID_918_"> <g> <g> <path  d="M366.781,73.842c-12.021,0-23.321,4.681-31.82,13.18c-17.546,17.545-17.546,46.094,0,63.639 c8.499,8.5,19.8,13.181,31.82,13.181s23.32-4.681,31.819-13.181c17.546-17.545,17.546-46.094,0-63.639 C390.101,78.522,378.801,73.842,366.781,73.842z M377.388,129.448c-2.833,2.833-6.601,4.394-10.606,4.394 c-4.007,0-7.774-1.561-10.607-4.394c-5.849-5.849-5.849-15.365,0-21.213c2.833-2.833,6.601-4.393,10.607-4.393 s7.773,1.56,10.606,4.393C383.236,114.083,383.236,123.6,377.388,129.448z"/> <path d="M472.704,21.533l-2.393-6.223l-6.224-2.393C441.801,4.346,418.406,0,394.556,0c-50.711,0-98.243,19.604-133.841,55.202 l-44.928,44.927l-113.138,14.142l-83.364,83.364l70.711,28.285l-15.734,15.735l22.835,22.835l-35.355,49.497l109.895,109.895 l49.497-35.356l22.835,22.835l15.734-15.735l28.284,70.711l83.364-83.364l14.141-113.136l44.928-44.929 C483.287,172.04,499.885,92.21,472.704,21.533z M72.67,186.679l43.915-43.916l64.65-8.081l-68.159,68.159L72.67,186.679z M174.878,384.698l-73.953-73.954l17.677-24.748l81.025,81.024L174.878,384.698z M342.858,369.038l-43.915,43.915l-16.163-40.406 l68.159-68.159L342.858,369.038z M409.208,203.694l-165.24,165.24L190.934,315.9l116.673-116.672l-21.213-21.213L169.721,294.688 l-53.033-53.033l165.24-165.24C311.859,46.484,351.858,30,394.556,30c17.909,0,35.516,2.9,52.435,8.63 C466.615,96.637,452.134,160.768,409.208,203.694z"/> <rect x="44.257" y="396.362" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -269.1277 172.9926)" width="59.999" height="30"/> <rect x="101.686" y="423.791" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 -121.683 857.1786)" width="30" height="59.999"/> <rect x="1.835" y="353.93" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -251.5493 130.5676)" width="59.999" height="30"/> </g> </g> </g> </svg>
-
         <h3>Fast by default</h3>
         <p>Gridsome automatically optimizes your website to load and perform fast. You get <strong>code-splitting & progressive image support</strong> built-in to get close to <strong>perfect Lighthouse scores.</strong>
         </p>     
       </Feature>
 
       <Feature>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
-
         <h3>PWA offline-ready</h3>
         <p>Gridsome loads only the critical HTML, CSS, so your site <strong>loads as fast as possible</strong>. Once loaded, the next pages are prefetched so clicking around the site feels incredibly fast. <strong>It even works offline.</strong>
         </p>
       </Feature>
 
       <Feature>
-        <svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 22C12 22 20 18 20 12V5L12 2L4 5V12C4 18 12 22 12 22Z" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M16 12L12 8L8 12" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 8V17" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M16 12L12 8L8 12" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>
-
         <h3>Simple, safe deployment</h3>
         <p>Gridsome sites are pre-rendered at build time and can be <strong>entirely hosted on a CDN</strong>. It can handle thousands to millions of hits without breaking - and at <strong>no expensive server costs.</strong>
         </p>     
@@ -38,15 +29,11 @@
 
 
       <Feature>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" class="icon feather feather-trending-up"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
-
         <h3>SEO-friendly SPA</h3>
         <p>Gridsome sites render as pure HTML before Vue.js is loaded and SPA kicks in. This makes search engines able to crawl content and give <strong>better SEO ranking</strong> because of the super fast loading.</p>
       </Feature>
 
       <Feature>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>
-
         <h3>Instant hot-reloading</h3>
         <p>Enjoy modern dev tools like Vue.js<vue-logo width="28" height="28" />,<br> GraphQL <graph-ql-logo color="#E535AB" width="20" height="20" /> & webpack<webpack-logo width="28" height="28" />. Get instant <strong>hot-reloading</strong> for any data change while developing. Gridsome makes building websites fun again.
         </p>
