ab4d154642c7b5f14c33971d62e8394ee258b476
Sergey <tunecatcher@yandex.ru>
2019-02-26 15:28:28 +0300

    Made with Gridsome

--- src/layouts/partials/Footer.vue
+++ src/layouts/partials/Footer.vue
@@ -20,6 +20,7 @@
       target="_blank"
     >Contributors 🌟</a>
     <g-link to="/logo">Logo & assets</g-link>
+    <img class="made-with-gridsome" src="@/assets/images/made-with-gridsome.png">
   </footer>
 </template>
 
@@ -31,6 +32,12 @@ footer {
 
   @media screen and (max-width: 750px) {
     padding-bottom: 90px;
+  }
+
+  .made-with-gridsome {
+    width: 120px;
+    margin-bottom: 0;
+    margin-left: 10px;
   }
 }
 </style>
