a83c07cb5dcc9a719123f18e98b929b8a16e13ea
Tommy Vedvik <tommyvedvik@gmail.com>
2019-05-01 00:07:15 +0200

    fix logo sizes for sponsors

--- src/components/home/HomeCommunity.vue
+++ src/components/home/HomeCommunity.vue
@@ -1,17 +1,24 @@
 <template>
   <Section class="post">
-
   	<h2 class="text-center mb-x2">Gridsome Sponsors</h2>
-
     <div class="grid-cols container-md grid-cols--2">
-    	<Card class="text-center">
-	      <g-image width="200" src="~/assets/images/logo-netlify.svg" alt="Netlify"/>
-	      <p>Thanks to <a href="https://netlify.com"  rel="noopener" target="_blank">Netlify</a></g-link> for sponsoring Gridsome hosting infrastructure. With Netlify every PR to Gridsome.org repo gets a preview URL automatically.</p>
+    	<Card class="sponsor text-center">
+	      <g-image src="~/assets/images/logo-netlify.svg" alt="Netlify"/>
+	      <p>Thanks to <a href="https://netlify.com"  rel="noopener" target="_blank">Netlify</a></g-link> for sponsoring Gridsome hosting infrastructure.</p>
     	</Card>
-      <Card class="text-center">
-	      <g-image width="200" src="~/assets/images/logo-algolia.svg" alt="Algolia"/>
-	      <p>Thanks to <a href="https://algolia.com"  rel="noopener" target="_blank">Algolia</a> for providing the search to Gridsome Plugins and Documentation</p>
+      <Card class="sponsor text-center">
+	      <g-image src="~/assets/images/logo-algolia.svg" alt="Algolia"/>
+	      <p>Thanks to <a href="https://algolia.com"  rel="noopener" target="_blank">Algolia</a> for providing the search to Plugins and Documentation</p>
     	</Card>
     </div>
   </Section>
 </template>
+
+<style lang="scss">
+.sponsor {
+	img {
+		max-height: 35px;
+		width: auto;
+	}
+}
+</style>
