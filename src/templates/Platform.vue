52c7e8aa5eaa368bafb2112a73a30a24fa1559cb
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-05-09 01:30:09 +0200

    fix starter platform page

--- src/templates/Platform.vue
+++ src/templates/Platform.vue
@@ -1,15 +1,53 @@
 <template>
   <Starters>
-    <h1>Platforms</h1>
+    <h1>{{ $page.platform.title }}</h1>
+
+    <div class="grid-cols grid-cols--3">
+      <StarterCard v-for="starter in $page.platform.belongsTo.edges" :key="starter.node.id" :node="starter.node"  />
+    </div>
   </Starters>
 </template>
 
+<page-query>
+query Platform($id: String!) {
+  platform(id: $id) {
+    title
+    belongsTo {
+      edges {
+        node {
+          ... on Starter {
+            id
+            title
+            description
+            preview
+            repo
+            platforms {
+              title
+              logo
+            }
+            author {
+              title
+              path
+            }
+            path
+            screenshot (width: 560, height: 664)
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
 import Starters from '~/layouts/Starters.vue'
+import StarterCard from '~/components/StarterCard.vue'
 
 export default {
   components: {
-    Starters
+    Starters,
+    StarterCard
   }
 }
 </script>
+
