3cca2c174b885fa40e279472aa7c6198a936f58e
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-05-14 21:09:55 +0200

    Revert "test using the system font stack"
    This reverts commit 4cd2b78d8e5c46d99dc1f662788ad4e2b9fa0d3d.

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ const typography = new Typography({
   baseFontSize: '18px',
   baseLineHeight: 1.6,
   scaleRatio: 1.9,
-  headerFontFamily: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
-  bodyFontFamily: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+  headerFontFamily: ['Jost', 'Helvetica','Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Jost', 'Helvetica','Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
 })
 
 export default function (Vue, { head, router, isServer }) {
