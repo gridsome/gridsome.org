4053d367d9fa341cca0cf4171027b76bca707d0e
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-09-12 23:25:46 +0200

    highlight page-query in html code blocks

--- gridsome.server.js
+++ gridsome.server.js
@@ -2,6 +2,15 @@ const path = require('path')
 const fs = require('fs-extra')
 const execa = require('execa')
 const yaml = require('js-yaml')
+const Prism = require('prismjs')
+
+// highlight page-query and static-query in html
+Prism.languages.html.graphql = {
+  pattern: /(<(page|static)-query[\s\S]*?>)[\s\S]*?(?=<\/(page|static)-query>)/i,
+  inside: Prism.languages.graphql,
+  lookbehind: true,
+  greedy: true
+}
 
 module.exports = function (api) {
   api.loadSource(async store => {
