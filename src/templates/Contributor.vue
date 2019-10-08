90ef67bb26cde3b35cf400c6a8d5fe0f203fa70b
Cody Barr <cody.barr@gmail.com>
2019-10-08 16:46:17 -0400

    Added twitter handles to contributor profiles (#268)

--- src/templates/Contributor.vue
+++ src/templates/Contributor.vue
@@ -8,6 +8,9 @@
         <p v-if="$page.contributor.bio" class="lead container-sm">
           {{ $page.contributor.bio }}
         </p>
+        <a v-if="$page.contributor.twitter" :href="`https://twitter.com/${$page.contributor.twitter}`">
+            {{ $page.contributor.twitter }}
+        </a>
       </div>
 
       <h3 class="text-center" v-if="$page.contributor.posts.edges.length">Blog posts</h3>
@@ -30,6 +33,7 @@ query Contributor ($id: ID!) {
     title
     bio
     avatar (width: 124)
+    twitter
     posts: belongsTo(filter: {typeName: {eq: BlogPost}}) {
       totalCount
       pageInfo {
