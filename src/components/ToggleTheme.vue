a6971c6731098c68062ff4ded7b85d31f6dfd95b
Tommy Vedvik <tommyvedvik@gmail.com>
2019-10-09 16:58:33 +0200

    better rotate

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -1,6 +1,6 @@
 <template>
-  <a role="button" aria-label="Toggle dark/light" title="Toggle dark/light" class="toggle-theme">
-    <svg v-if="theme === 'light'" @click.prevent="toggleTheme('dark')"  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
+  <a role="button" @click.prevent="toggleTheme()" aria-label="Toggle dark/light" title="Toggle dark/light" class="toggle-theme">
+    <svg v-if="theme === 'light'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
       <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -12,25 +12,28 @@
       <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
     </svg>
     
-    <svg v-else-if="theme === 'dark'" @click.prevent="toggleTheme('sepia')"  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
+    <svg v-else-if="theme === 'dark'"  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon">
       <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
 
-    <svg v-else-if="theme === 'sepia'" @click.prevent="toggleTheme('light')" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud-snow"><path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path><line x1="8" y1="16" x2="8" y2="16"></line><line x1="8" y1="20" x2="8" y2="20"></line><line x1="12" y1="18" x2="12" y2="18"></line><line x1="12" y1="22" x2="12" y2="22"></line><line x1="16" y1="16" x2="16" y2="16"></line><line x1="16" y1="20" x2="16" y2="20"></line></svg>
+    <svg v-else-if="theme === 'sepia'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud-snow"><path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path><line x1="8" y1="16" x2="8" y2="16"></line><line x1="8" y1="20" x2="8" y2="20"></line><line x1="12" y1="18" x2="12" y2="18"></line><line x1="12" y1="22" x2="12" y2="22"></line><line x1="16" y1="16" x2="16" y2="16"></line><line x1="16" y1="20" x2="16" y2="20"></line></svg>
   </a>
 </template>
 
 <script>
+let themes = ['light', 'dark', 'sepia']
 export default {
   data() {
     return {
-      theme: 'light'
+      theme: 'light',
+      current: 0
     }
   },
   methods: {
-    toggleTheme(theme) {
-      this.theme = theme
-      window.__setPreferredTheme(theme)
+    toggleTheme() {
+      this.current = (this.current + 1) % 3
+      window.__setPreferredTheme(themes[this.current])
+      this.theme = themes[this.current]
     }
   },
   mounted() {
