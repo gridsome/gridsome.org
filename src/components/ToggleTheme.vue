e331f87e53aba2b21103b9b4d1fd817d4a0915f8
Tommy Vedvik <tommyvedvik@gmail.com>
2019-10-09 20:37:48 +0200

    .

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -1,5 +1,9 @@
 <template>
-  <a role="button" @click.prevent="toggleTheme()" aria-label="Toggle dark/light" title="Toggle dark/light" class="toggle-theme">
+  <a role="button" @click.prevent="toggleTheme()"  
+      :aria-label="'Toggle ' + nextTheme" 
+      :title="'Toggle ' + nextTheme"
+      class="toggle-theme"
+    >
     <svg v-if="theme === 'light'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
@@ -16,15 +20,7 @@
       <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
 
-    <svg v-else-if="theme === 'sepia'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sunrise">
-      <path d="M17 18a5 5 0 0 0-10 0"></path>
-      <line x1="12" y1="7" x2="12" y2="6"></line>
-      <line x1="4.22" y1="10.22" x2="5.64" y2="11.64"></line>
-      <line x1="1" y1="18" x2="3" y2="18"></line>
-      <line x1="21" y1="18" x2="23" y2="18"></line>
-      <line x1="18.36" y1="11.64" x2="19.78" y2="10.22"></line>
-      <line x1="23" y1="22" x2="1" y2="22"></line>
-    </svg>
+    <svg v-else-if="theme === 'sepia'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>
   </a>
 </template>
 
@@ -35,6 +31,13 @@ export default {
   data() {
     return {
       theme: 'light',
+    }
+  },
+  computed: {
+    nextTheme() {
+      const currentIndex = themes.indexOf(this.theme)
+      const nextIndex = (currentIndex + 1) % themes.length
+      return themes[nextIndex]
     }
   },
   methods: {
