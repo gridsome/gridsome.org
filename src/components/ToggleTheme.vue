e195cdc696e409dd923ef7ba60a4d6f36aeb4593
Tommy Vedvik <tommyvedvik@gmail.com>
2019-10-09 20:28:24 +0200

    Update ToggleTheme.vue

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -16,7 +16,15 @@
       <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
 
-    <svg v-else-if="theme === 'sepia'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cloud-snow"><path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path><line x1="8" y1="16" x2="8" y2="16"></line><line x1="8" y1="20" x2="8" y2="20"></line><line x1="12" y1="18" x2="12" y2="18"></line><line x1="12" y1="22" x2="12" y2="22"></line><line x1="16" y1="16" x2="16" y2="16"></line><line x1="16" y1="20" x2="16" y2="20"></line></svg>
+    <svg v-else-if="theme === 'sepia'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sunrise">
+      <path d="M17 18a5 5 0 0 0-10 0"></path>
+      <line x1="12" y1="7" x2="12" y2="6"></line>
+      <line x1="4.22" y1="10.22" x2="5.64" y2="11.64"></line>
+      <line x1="1" y1="18" x2="3" y2="18"></line>
+      <line x1="21" y1="18" x2="23" y2="18"></line>
+      <line x1="18.36" y1="11.64" x2="19.78" y2="10.22"></line>
+      <line x1="23" y1="22" x2="1" y2="22"></line>
+    </svg>
   </a>
 </template>
 
@@ -27,20 +35,19 @@ export default {
   data() {
     return {
       theme: 'light',
-      current: 0
     }
   },
   methods: {
     toggleTheme() {
-      this.current = (this.current + 1) % 3
-      
-      window.__setPreferredTheme(themes[this.current])
-      this.theme = themes[this.current]
+      const currentIndex = themes.indexOf(this.theme);
+      const nextIndex = (currentIndex + 1) % themes.length;
+      window.__setPreferredTheme(themes[nextIndex])
+      this.theme = themes[nextIndex]
     }
   },
-  mounted() {
+  async mounted() {
     // set default
-    if (window.__theme) this.theme = window.__theme
+    if (typeof window.__theme !== 'undefined') this.theme = window.__theme
   }
 }
 </script>
