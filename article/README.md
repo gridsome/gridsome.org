583dd62513ee11fcfb1cd542366a8160fb7079ef
linuxing3 <linuxing3@qq.com>
2020-02-27 09:24:05 -0400

    added slug

---
+++ article/README.md
@@ -0,0 +1,8 @@
+---
+title: "Articles"
+author: "linuxing3"
+datae: "2020-02-27"
+
+---
+
+# Article List
--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -13,7 +13,7 @@
 </template>
 
 <page-query>
-query ArticlePage {
+query ArticlePages {
   articles: allArticlePage {
     edges {
       node {
